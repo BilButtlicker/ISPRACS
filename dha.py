692beaf96ff40137a34c15f9718510a1c254ad13
shari111 <121186566+shari111@users.noreply.github.com>
2022-12-22 22:37:28 +0530

    Update dha.py

--- dha.py
+++ dha.py
@@ -1,1 +1,16 @@
+A = int(input("Enter Alice's number:"))
+B = int(input("Enter Bob's number:"))
+g = int(input("Enter base value g:"))
+p = int(input("Enter mod value p:"))
+
+Xa = (g**A)%p
+Xb = (g**B)%p
+
+print("Xa is ",Xa)
+print("Xb is ",Xb)
+
+x1 = (g**Xb)%p
+x2 = (g**Xa)%p
+
+print(x1,x2)
 
